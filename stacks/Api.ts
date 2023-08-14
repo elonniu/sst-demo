84f6d31f2345790233e09da5b1743dffda4389f9
Jingyi Niu <elonniu@amazon.com>
2023-08-14 08:52:23 +0800

    disable py3.10

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -68,12 +68,12 @@ export function Api({stack, app}: StackContext) {
                     architecture: "x86_64",
                 }
             },
-            "GET /python": {
-                function: {
-                    handler: "resources/python/lambda.handler",
-                    runtime: "python3.10",
-                }
-            },
+            // "GET /python": {
+            //     function: {
+            //         handler: "resources/python/lambda.handler",
+            //         runtime: "python3.10",
+            //     }
+            // },
             "GET /proxy": {
                 type: "url",
                 url: "https://www.aws.com",
